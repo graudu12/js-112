cfc3f2cf62211dd88677bc45d360663b930715d9
Olexiy-repin <repinlyoha@gmail.com>
2024-10-28 21:28:25 +0200

    classwork

---
+++ js/01-vars.js
@@ -0,0 +1,45 @@
+/*
+ * Змінні та типи даних
+ *
+ * - Оголошення змінних з let и const
+ * - Перевизначення значення
+ * - Найменування змінних
+ * - Вибір між const і let
+ * - Виведення даних
+ * - Отримання даних
+ * - Примітивні типи даних: number, string, boolean, null, undefined, Symbol, BigInt
+ * - Оператор typeof
+ * - Суворий режим
+ */
+
+// const pricePerItem = 10;
+
+// console.log(pricePerItem);
+
+// pricePerItem = 30;
+
+// console.log(pricePerItem);
+
+// const firstName = prompt('Введіть ваше імʼя');
+
+// console.log(firstName);
+
+// const isAdult = confirm('Чи є вам 18 років?');
+
+// console.log(isAdult);
+
+// alert('Hello world!');
+
+// const someNumber = prompt('Введіть якесь число!');
+
+// console.log(typeof someNumber);
+
+// const someString = '10';
+// const someNumber = 10;
+
+// console.log(someString);
+// console.log(someNumber);
+
+// userAge = 30;
+
+// console.log(userAge);
---
+++ js/02-operators.js
@@ -0,0 +1,55 @@
+/*
+ * Арифметичні операції
+ * - Оператори +, -, *, /, %, **
+ * - Комбіновані оператори
+ */
+
+// const numA = 10;
+// const numB = 3;
+
+// const result = numA ** numB;
+
+// console.log(result);
+
+/*
+10 / 4 = 4 + 4
+*/
+
+/*
+ ? Виведи на екран загальну кількість яблук та винограду. Різницю яблук та винограду.
+ */
+// const apples = 135;
+// const grapes = 47;
+
+// const totalFruits = apples + grapes;
+
+// console.log(totalFruits);
+
+// const diff = apples - grapes;
+
+// console.log(diff);
+
+/*
+ ? Заміни вираз перевизначення комбінованим оператором
+ */
+
+//? Без комбіновоного оператора
+// let students = 100;
+
+// students = students + 50;
+
+// console.log(students);
+
+//? З використанням комбінованого оператора
+// let students = 100;
+
+// students += 50;
+
+// console.log(students);
+
+/*
+ ? Розбери пріоритет операторів в інструкції привласнення значення змінної result
+ */
+// const result = 108 + (223 - 2) * 5;
+
+// console.log(result);
