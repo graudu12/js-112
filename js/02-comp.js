a5dbe5ca456070fec7d73d3094ad8434e8f17565
Olexiy-repin <repinlyoha@gmail.com>
2024-10-30 21:32:05 +0200

    classwork

---
+++ js/02-comp.js
@@ -0,0 +1,28 @@
+/*
+ * Оператори
+ *
+ * - Оператори порівняння
+ * - Оператори рівності
+ */
+
+// console.log(5 >= 4);
+
+// console.log(10 >= '7');
+
+// console.log('2' > '22');
+
+// console.log('papaya' <= 'papaya');
+
+// console.log('4' == 4);
+
+// console.log('6' === 6);
+
+// console.log('false' === false);
+
+// console.log(1 == true);
+
+// console.log(1 === true);
+
+// console.log('0' == false);
+
+// console.log('0' === false);
---
+++ js/03-numbers.js
@@ -0,0 +1,51 @@
+/*
+ * Арифметичні функції
+ *
+ * - Number.parseInt
+ * - Number.parseFloat
+ * - Math
+ */
+
+/*
+  ? Попросіть користувача ввести число.
+  ? Виведіть результат у консоль.
+  ? Приведіть рядок до числа.
+*/
+// const userInput = Number(prompt('Enter your number'));
+
+// console.log(userInput);
+
+//? Дізнайтесь числове значення ширини елемента.
+// const elementWidth = '50px';
+// const numericalWidth = Number.parseInt(elementWidth);
+
+// console.log('elementWidth:', elementWidth);
+// console.log('numericalWidth:', numericalWidth);
+
+//? Дізнайтесь числове значення висоти елемента.
+// const elementHeight = '200.74px';
+// const numericalHeight = Number.parseFloat(elementHeight);
+
+// console.log('elementHeight: ', elementHeight);
+// console.log('numericalHeight: ', numericalHeight);
+
+//? Попросіть користувача ввести два числа, складіть їх та виведіть результат у консоль.
+// const numA = Number(prompt('Enter first number'));
+// const numB = Number(prompt('Enter second number'));
+
+// const result = numA + numB;
+
+// console.log(result);
+
+/*
+ ? Напиши скрипт, який виводить у консоль заокруглені вгору/вниз значення
+ ? змінної value. Використовуй методи Math.floor(), Math.ceil()
+ ? та Math.round(). Перевір що буде в консолі при значеннях 27.3 та 27.9.
+ */
+// let value = 27.1;
+
+// console.log(value);
+
+// value = Math.ceil(value);
+
+// console.log(value);
